7f1aad4692ca2a6e02330358ca32fd7bd23956ac
Zakary Mitrovic <1944315@cstj.qc.ca>
2022-11-07 08:04:42 -0500

    second commit

---
+++ src/components/App/App.js
@@ -0,0 +1,11 @@
+import {
+    RouterProvider,
+    createBrowserRouter,
+    Navigate,
+} from "react-router-dom";
+import Layout from "../Layout/Layout";
+
+const App = () =>{
+    
+}
+export default App;
---
+++ src/config/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD7u2_PLD4wAg-ec7qlblAfxeh7fAe57Sw",
+  authDomain: "synthese-aacf7.firebaseapp.com",
+  projectId: "synthese-aacf7",
+  storageBucket: "synthese-aacf7.appspot.com",
+  messagingSenderId: "1061528563086",
+  appId: "1:1061528563086:web:d147a83ca2d36acf0688df"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
